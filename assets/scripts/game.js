91c59b6b5b69467766e94143ae8f83c521640e99
doker123 <dodo2larin@gmail.com>
2025-10-17 10:04:46 +0700

    Переписал маин и начал game.js

---
+++ assets/scripts/game.js
@@ -0,0 +1,51 @@
+class Drawable {
+    constructor(game) {
+        this.game = game;
+        this.x = 0;
+        this.y = 0;
+        this.w = 0;
+        this.h = 0;
+        this.offsets = {
+            x: 0,
+            y: 0
+        }
+    }
+
+    update() {
+        this.x += this.offsets.x;
+        this.y += this.offsets.y;
+    }
+
+    createElement() {
+        this.element = document.createElement("div");
+        this.element.className = "element " + this.constructor.name.toLowerCase();
+        $('.elements').append(this.element);
+    }
+
+    draw() {
+        this.element.style = `
+        left; ${this.x}px;
+        top; ${this.y}px;
+        width: ${this.w}px;
+        height: ${this.h}px;
+        `;
+    }
+    removeElement() {
+        this.element.remove();
+    }
+    isCollision(element) {
+        let a = {
+            x1: this.x,
+            y1: this.y,
+            x2: this.x + this.w,
+            y2: this.y + this.h,
+        }
+        let b = {
+            x1: element.x,
+            y1: element.y,
+            x2: element.x + element.w,
+            y2: element.y + element.h,
+        }
+        return a.x1 < b.x2 && b.x1 < a.x2 && a.y1 < b.y2 && b.y1 < a.y2;
+    }
+}
